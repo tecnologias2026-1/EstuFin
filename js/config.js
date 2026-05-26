77ed79196a7cae4d0e703e08da7f4cfe10de3078
Jhon Sebastian Blandon Perez <est.jhon.sblandon@unimilitar.edu.co>
2026-05-26 01:26:46 -0500

    Conectar frontend con backend Render

--- js/config.js
+++ js/config.js
@@ -1,2 +1,3 @@
-const API_BASE = 'http://127.0.0.1/back-estuFin/api';
+//const API_BASE = 'http://127.0.0.1/back-estuFin/api';
 //const API_BASE = 'http://localhost/back-estuFin/api';
+const API_URL = "https://back-estufin.onrender.com";
