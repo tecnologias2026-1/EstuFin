8cdcc328d9332c8c7927f4b4390bbf5eca58f80b
Jhon Sebastian Blandon Perez <est.jhon.sblandon@unimilitar.edu.co>
2026-05-26 02:38:10 -0500

    Fix API_BASE en config

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 //const API_BASE = 'http://127.0.0.1/back-estuFin/api';
 //const API_BASE = 'http://localhost/back-estuFin/api';
 const API_URL = "https://back-estufin.onrender.com";
+const API_BASE = "https://back-estufin.onrender.com/api";
